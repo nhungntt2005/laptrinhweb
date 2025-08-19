336673e5025d04f5d0cd15eef65b850768ee94d0
nhungntt2005 <nhung.ntt2005@gmail.com>
2025-08-19 15:58:14 +0700

    Update bai1.php

--- bai1.php
+++ bai1.php
@@ -1,3 +1,12 @@
 <?php
 echo "<strong>hello</strong>, world";
+echo '<br><br><a href="index.php" style="
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #3498db;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  font-weight: bold;
+">🏠 Quay lại trang chủ</a>';
 ?>
